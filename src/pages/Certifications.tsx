24e0250f57a3d8e6725deba5b94ad9cbc44f817c
Anushka1357 <152285369+Anushka1357@users.noreply.github.com>
2025-09-25 02:59:21 +0530

    Update Certifications.tsx

--- src/pages/Certifications.tsx
+++ src/pages/Certifications.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Certifications.css';
 import { FaExternalLinkAlt, FaUniversity } from 'react-icons/fa';
 import { SiCoursera, SiIeee, SiOracle } from 'react-icons/si';
